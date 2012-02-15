e7376692cd71ab45bf229d33d275c7bdb1331847
Adam Coddington <me@adamcoddington.net>
2012-02-15 12:38:28 -0800

    Adding license and setup script.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+from teamspeak3 import get_version
+
+setup(
+    name='teamspeak3',
+    version=get_version(),
+    url='http://bitbucket.org/latestrevision/python-teamspeak3/',
+    description='Interact with teamspeak with Python',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['teamspeak3',],
+    install_requires = []
+)
+
