5b944a6997c72952493854b1ba3c7074e17efdd6
Piotr Majkrzak <petrol.91@gmail.com>
2012-08-06 22:52:35 +0200

    Corect replacement

--- teamspeak3/__init__.py
+++ teamspeak3/__init__.py
@@ -22,7 +22,7 @@ from client import *
 from message import *
 from exceptions import *
 
-__version__ = (1, 4, 4, )
+__version__ = (1, 4, 5, )
 
 def get_version():
     return '.'.join(str(bit) for bit in __version__)
